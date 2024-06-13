9724b295f71ccd32a67ab2ed13f29c75334a2482
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-06-13 16:56:49 +0530

    env added

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,4 @@
-// export const BACKEND_URL = "http://127.0.0.1:8787"
-export const BACKEND_URL = "https://backend.deevee4712.workers.dev"
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const BACKEND_URL = process.env.DATABASE_URL;
